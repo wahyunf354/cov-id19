7ae96de9f0a2e85eb7d97af0ad638d9ea723c2f0
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-05-11 10:52:07 +0700

    install lib js yang diperlukan

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      /* style and css loader */
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  /* plugin */
+  plugins: [
+    /* HTML Webpack Plugin */
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    // new ExtractTextPlugin("styles.css"),
+  ],
+};
